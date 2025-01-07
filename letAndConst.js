146b07f17b50c0b168fb2a08abd7d9b5421f6caf
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-07 18:40:00 +0500

    added shadowing

--- letAndConst.js
+++ letAndConst.js
@@ -1,11 +1,11 @@
-// console.log(x);
+// // console.log(x);
 
-let x = 3; /// will try to find in memory and it is not present there
-let b;
-b = 19;
-console.log(b); // will get value
+// let x = 3; /// will try to find in memory and it is not present there
+// let b;
+// b = 19;
+// console.log(b); // will get value
 
-const a; // will throwsyntax error
-a = 20; // cant do this with const
+// const a; // will throwsyntax error
+// a = 20; // cant do this with const
 
-console.log(a);
+// console.log(a);
---
+++ shadowing.js
@@ -0,0 +1,10 @@
+var a = 10;
+
+{
+  var a = 30;
+  console.log(a);
+}
+
+console.log(a);
+
+a();
