c65cbf78bb87c74f9af64602b9970446e3e803e0
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-07 13:49:22 +0500

    added let and const difference

---
+++ letAndConst.js
@@ -0,0 +1,11 @@
+// console.log(x);
+
+let x = 3; /// will try to find in memory and it is not present there
+let b;
+b = 19;
+console.log(b); // will get value
+
+const a; // will throwsyntax error
+a = 20; // cant do this with const
+
+console.log(a);
