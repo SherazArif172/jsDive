467ac0e09638bc5579106ff0fc9a81f8e12ff527
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-08 11:34:57 +0500

    added deep logic of clsure

--- closure.js
+++ closure.js
@@ -1,10 +1,26 @@
+// function a() {
+//   var x = 10;
+//   function b() {
+//     console.log(x);
+//   }
+//   return x;
+// }
+
+// a();
+// console.log(a()); // even the function has been returned still it prints the value of x , this is called closure
+
 function a() {
   var x = 10;
   function b() {
     console.log(x);
+    var y = 30;
+    function c() {
+      console.log(x, y);
+    }
+    c();
   }
-  return x;
+  b();
 }
-
 a();
-console.log(a()); // even the function has been returned still it prints the value of x , this is called closure
+
+// it still follows the closure property
