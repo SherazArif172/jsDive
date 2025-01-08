15d67b932ad1b2658cad65d7aea698063067a017
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-08 15:13:13 +0500

    added changes

--- timeout+closure.js
+++ timeout+closure.js
@@ -1,9 +1,10 @@
-// function abc() {
-//   for (var i = 0; 1 <= 5; i++) {
-//     setTimeout(function() => {
-//       console.log(i);
-//     }, i * 1000);
-//   }
-// }
+function abc() {
+  for (var i = 1; 1 <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, 1000);
+  }
+  console.log("All is well");
+}
 
-// abc();
+abc();
