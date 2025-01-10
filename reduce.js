8840a89c433b480611efb8e40151d7c5d0c2228a
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-10 16:59:20 +0500

    aded reduce

---
+++ reduce.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5, 6];
+
+Array.prototype.myreduce = function () {
+  var sum = 0;
+  for (let index = 0; index < this.length; index++) {
+    sum = sum + this[index];
+  }
+  return sum;
+};
+
+console.log(num.myreduce());
