620b588a5767990afa86b588480c52f2e0911468
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-13 14:19:18 +0500

    added

--- reduce.js
+++ reduce.js
@@ -1,13 +1,13 @@
-const num = [1, 2, 3, 4, 5, 6];
+// const num = [1, 2, 3, 4, 5, 6];
 
-Array.prototype.myreduce = function () {
-  var sum = 0;
-  for (let index = 0; index < this.length; index++) {
-    sum = sum + this[index];
-  }
-  return sum;
-};
+// Array.prototype.myreduce = function () {
+//   var sum = 0;
+//   for (let index = 0; index < this.length; index++) {
+//     sum = sum + this[index];
+//   }
+//   return sum;
+// };
 
-console.log(num.myreduce());
+// console.log(num.myreduce());
 
-num.filter();
+// num.filter();
