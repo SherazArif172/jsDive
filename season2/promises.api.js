46083bdc6df3d543822764ad005455ec5e86dd04
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-16 13:20:24 +0500

    added race

--- season2/promises.api.js
+++ season2/promises.api.js
@@ -6,14 +6,14 @@ const p1 = new Promise(function (resolve, reject) {
 const p2 = new Promise(function (resolve, reject) {
   setTimeout(() => {
     resolve("p1 succes");
-  }, 1000);
+  }, 2000);
 });
 const p3 = new Promise(function (resolve, reject) {
   setTimeout(() => {
     reject("p3 rejected");
-  }, 2000);
+  }, 1000);
 });
 
-Promise.allSettled([p1, p2, p3])
+Promise.race([p1, p2, p3])
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
