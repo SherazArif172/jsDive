88027e4649b3d281f588a39b151a0b875455da5d
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-16 13:12:46 +0500

    added allsetteled

--- season2/promises.api.js
+++ season2/promises.api.js
@@ -14,6 +14,6 @@ const p3 = new Promise(function (resolve, reject) {
   }, 2000);
 });
 
-Promise.all([p1, p2, p3])
+Promise.allSettled([p1, p2, p3])
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
