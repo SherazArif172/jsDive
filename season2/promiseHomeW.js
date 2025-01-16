e7dcc3115cfaf1c7e25700cc9b7e52a13bc3c8c5
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-16 12:47:32 +0500

    adding all thechanges

--- season2/promiseHomeW.js
+++ season2/promiseHomeW.js
@@ -1,29 +1,39 @@
-const cart = [1, 2, 3, 4, 5];
+// const cart = [1, 2, 3, 4, 5];
 
-createOrder(orderId)
-  .then(function (resolve, reject) {
-    console.log(orderId);
-    return orderId;
-  })
-  .catch(function (err) {
-    console.log("error while ", err);
-  });
+// createOrder(cart)
+//   .then(function (orderId) {
+//     console.log(orderId);
+//     return orderId;
+//   })
+//   .then(function (orderId) {
+//     proceedToPayment(orderId);
+//   })
+//   .catch(function (err) {
+//     console.log("error while ", err);
+//   });
 
-function createOrder(cart) {
-  const pr = new Promise(function (resolve, reject) {
-    if (!invalidCart(cart)) {
-      const err = new Error("cart is invalid");
-      reject(err);
-    }
+// function createOrder(cart) {
+//   const pr = new Promise(function (resolve, reject) {
+//     if (!invalidCart(cart)) {
+//       const err = new Error("cart is invalid");
+//       reject(err);
+//       return;
+//     }
 
-    const orderId = "1234567";
-    if (orderId) {
-      resolve(orderId);
-    }
-  });
-  return pr;
-}
+//     const orderId = "1234567";
+//     if (orderId) {
+//       resolve(orderId);
+//     }
+//   });
+//   return pr;
+// }
 
-function invalidCart(cart) {
-  return true;
-}
+// function proceedToPayment(orderId) {
+//   return new Promise(function (resolve, reject) {
+//     resolve("payment added");
+//   });
+// }
+
+// function invalidCart(cart) {
+//   return true;
+// }
