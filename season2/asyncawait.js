2038250c0506081dde3f0645d9494bf88e6401f4
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-20 13:00:59 +0500

    added async await

---
+++ season2/asyncawait.js
@@ -0,0 +1,8 @@
+async function namee() {
+  return "ali ijaz";
+}
+
+const dataPromsie = namee();
+// console.log(dataPromsie);
+
+dataPromsie.then((res) => console.log(res));
--- season2/promises.api.js
+++ season2/promises.api.js
@@ -1,19 +1,19 @@
-const p1 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject("p1 rejected");
-  }, 3000);
-});
-const p2 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject("p2 rejected");
-  }, 2000);
-});
-const p3 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject("p3 rejected");
-  }, 1000);
-});
+// const p1 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     reject("p1 rejected");
+//   }, 3000);
+// });
+// const p2 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     reject("p2 rejected");
+//   }, 2000);
+// });
+// const p3 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     reject("p3 rejected");
+//   }, 1000);
+// });
 
-Promise.any([p1, p2, p3])
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err), console.log(err.errors));
+// Promise.any([p1, p2, p3])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err), console.log(err.errors));
