84cc0981ea91212639b022c8cc69c6c44a3d50d7
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-20 13:15:17 +0500

    addd async await comb

--- season2/asyncawait.js
+++ season2/asyncawait.js
@@ -2,6 +2,8 @@ const p = new Promise((resolve, reject) => {
   resolve("value is this");
 });
 
+// returning promise
+
 async function namee() {
   return p;
 }
@@ -10,3 +12,18 @@ const dataPromsie = namee();
 // console.log(dataPromsie);
 
 dataPromsie.then((res) => console.log(res));
+
+//// async await combo
+
+// simple way to deal with async
+async function dataa() {
+  return p.then((res) => console.log(res));
+}
+
+dataa();
+
+// using async await combo
+
+async function dataaa() {
+  return await p.then((res) => console.log(res));
+}
