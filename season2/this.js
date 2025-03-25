8690c210876674c5a4264047a6b726db300369f2
SherazArif172 <sherazarifofficial@gmail.com>
2025-03-25 05:53:08 +0500

    added everything

--- season2/this.js
+++ season2/this.js
@@ -1,41 +1,41 @@
-// globl env
+// // globl env
 
-// console.log(this);
+// // console.log(this);
 
-// // this inside a function
+// // // this inside a function
 
-// function any() {
-//   console.log(this);
-// }
+// // function any() {
+// //   console.log(this);
+// // }
 
-// any();
+// // any();
+
+// // const obj = {
+// //   a: 10,
+// //   x: function () {
+// //     console.log(this.a);
+// //   },
+// // };
+
+// // obj.x();
+
+// // const ob2 = {
+// //   a: "ali ijaz",
+// // };
+
+// // obj.x.call(ob2);
 
 // const obj = {
 //   a: 10,
 //   x: function () {
-//     console.log(this.a);
+//     const y = function () {
+//       const z = () => {
+//         console.log(this);
+//       };
+//       z();
+//     };
+//     y();
 //   },
 // };
 
 // obj.x();
-
-// const ob2 = {
-//   a: "ali ijaz",
-// };
-
-// obj.x.call(ob2);
-
-const obj = {
-  a: 10,
-  x: function () {
-    const y = function () {
-      const z = () => {
-        console.log(this);
-      };
-      z();
-    };
-    y();
-  },
-};
-
-obj.x();
